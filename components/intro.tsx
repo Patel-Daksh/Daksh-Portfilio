f9a2660feb47b39f100601ad6011cf0ca77aad7f
Daksh Patel <patel.daks@northeastern.edu>
2024-03-28 21:39:32 -0400

    commit

--- components/intro.tsx
+++ components/intro.tsx
@@ -31,7 +31,7 @@ export default function Intro() {
             }}
           >
             <Image
-              src="/Daksh.png"
+              src="public/DakshImage.png"
               alt="Daksh portrait"
               width="350"
               height="350"
